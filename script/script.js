46345f708497f1aef206efa7dcd16e0994954c98
Sion Lee <sionlee29@gmail.com>
2023-12-12 14:39:55 -0500

    added clouds image, also running and jumping

--- script/script.js
+++ script/script.js
@@ -1,15 +1,52 @@
 window.onload = function(){
   const stickman = document.getElementById("stickman");
+  const obstacle = document.getElementById("obstacle");
+  let end = true;
 
   function jump() {
-    stickman.classList.add("jump");
+    end = false;
+    stickman.style.animation = 'jump 0.7s linear';
     
     setTimeout(function (){
-      stickman.classList.remove("jump");
+      stickman.style.animation = 'run 0.8s steps(8) infinite';
+      end = true
     }, 700);
   }
 
+  let isAlive = setInterval(function (){ 
+    //current y position of stickman
+    let stickmanTop = parseInt(window.getComputedStyle(stickman).getPropertyValue("top"));
+    //console.log(stickmanTop);
+    //current x position of obstacle
+    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    console.log(obstacleLeft);
+    //check for collision
+    if (obstacleLeft < 50 && obstacleLeft > 0 && stickmanTop >= 180){
+      alert("game over!");
+    }
+  }, 10)
+
   document.addEventListener("keydown", function (event) {
-    jump();
+    // checking if previous jump animation has ended.
+    if (end){
+      jump();
+    }
   });
+
+  //keep the game speed same for any framerate monitor
+  let prevTime = null;
+  function gameLoop(currentTime){
+    if(prevTime == null){
+      prevTime = currentTime;
+      requestAnimationFrame(gameLoop);
+      return;
+    }
+    const delta = currentTime-prevTime;
+    prevTime = currentTime;
+    requestAnimationFrame(gameLoop);
+    
+  }
+
+  requestAnimationFrame(gameLoop);
 }
+
